5bb986104fe02afa983b08e1558b585eeb652b4d
unknown <khanakbar0233@gmail.com>
2024-04-17 11:20:13 +0500

    Initial commit

---
+++ lib/disc_rolling.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DiscRolling extends StatefulWidget {
+  const DiscRolling({super.key});
+
+  @override
+  State<DiscRolling> createState() => _DiscRollingState();
+}
+
+class _DiscRollingState extends State<DiscRolling> {
+  var leftDice = 1;
+  var rightDice = 1;
+  click() {
+    setState(() {
+      leftDice = Random().nextInt(6)+1;
+      rightDice = Random().nextInt(6)+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Disc Rolling $leftDice',
+          style:const TextStyle(color: Colors.limeAccent),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                  child: GestureDetector(
+                      onTap: () {
+                        click();
+                      },
+                      child: Image(
+                          image: AssetImage("images/dice$leftDice.png")))),
+              Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      click();
+                    },
+                   child: Image(image: AssetImage("images/dice$rightDice.png")),
+              )),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: GestureDetector(
+                      onTap: () {
+                        click();
+                      },
+                      child: Image(
+                          image: AssetImage("images/dice$leftDice.png")))),
+              Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      click();
+                    },
+                    child: Image(image: AssetImage("images/dice$rightDice.png")),
+                  )),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
